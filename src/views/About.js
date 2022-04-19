6d9f56c910bbf778a59647286b4f6ddcbc541af6
JohnnyMohawk <johnnelsonalden@gmail.com>
2022-04-18 22:55:51 -0500

    troubleshoot deployment issue

--- src/views/About.js
+++ src/views/About.js
@@ -1,12 +1,11 @@
 import React from 'react'
 
 import PageHeader from '../components/PageHeader'
-import LazyImage from '../components/LazyImage'
 import Content from '../components/Content.js'
 import './About.css'
 
 export default ({ fields }) => {
-  const { title, subtitle, featuredImage, section1, section2 } = fields
+  const { title, subtitle, featuredImage, section1 } = fields
   return (
     <div className='About'>
       <PageHeader
@@ -19,13 +18,6 @@ export default ({ fields }) => {
           <Content source={section1} />
         </div>
       </div>
-      {/* <div className='section'>
-        <div className='container'>
-          <Content source={section2} />
-          <p>The image below is a {'<LazyImage />'}</p>
-          <LazyImage src={featuredImage} alt='LazyImage' />
-        </div>
-      </div> */}
     </div>
   )
 }
