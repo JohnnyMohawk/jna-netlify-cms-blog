6e43b92c2ae49da30f06e52449bd06bc29e80e3e
JohnnyMohawk <johnnelsonalden@gmail.com>
2022-04-25 21:07:48 -0500

    add link to portfolio site from blog homepage

--- src/views/Home.js
+++ src/views/Home.js
@@ -25,7 +25,10 @@ export default ({ fields }) => {
             <Link to={slugify(`/about`)} className='homeLinks'>who the f*ck is Johnny Mohawk?</Link> <br/><br/> 
 
             Don't care? Continue to the&nbsp;
-            <Link to={slugify(`/blog`)} className='homeLinks'>bloggery</Link>.
+            <Link to={slugify(`/blog`)} className='homeLinks'>bloggery</Link>.<br/><br/>
+
+            Check out more of my work&nbsp;
+            <a href="https://www.johnnelsonalden.com/" target="_blank" rel="noopenner noreferrer" className='homeLinks'>here</a>.
           </p>
         </div>
       </div>
